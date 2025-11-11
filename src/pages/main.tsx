abbb0d43fae62d77ec8311b3e3acd28582bb12d0
Vitor Hugo <virtualpage.dev@gmail.com>
2025-11-11 09:37:58 -0300

    convite expirado para karen e rosa

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -2,9 +2,8 @@ import { useEffect, useState, useRef } from "react";
 import { WindowMsn } from "../components/window-msn";
 import { WindowNotification } from "../components/window-notitication";
 import { WindowTintemo } from "../components/window-trintemo";
-import { getGuests } from "../services/services"; // Importa notConfirmation
-import { useChat } from "../context/chat-context";
-import Countdown from "react-countdown";
+import { getGuests } from "../services/services";
+import { useChat } from "../context/chat-context";;
 import { FaCheck } from "react-icons/fa6";
 
 type Guest = { slug: string };
@@ -16,8 +15,7 @@ export const Main = () => {
     const [showNotification, setShowNotification] = useState(false);
     const audioRef = useRef<HTMLAudioElement>(null);
     const [guest, setGuest] = useState<Guest[]>([]);
-    const { currentGuest } = useChat(); // Adiciona setConfirmed e setCurrrentGuest do contexto
-
+    const { currentGuest } = useChat();
     const step = currentGuest?.step ?? 0;
 
     useEffect(() => {
@@ -73,7 +71,7 @@ export const Main = () => {
                     </div>
                 ) : (
                     <div className="flex w-full flex-col items-center justify-center md:gap-6">
-                        {showMessage && <WindowMsn close={() => setShowMessage(false)} />}
+                        {showMessage && (currentGuest?.slug !== "rosa-e-gi" && currentGuest?.slug !== "karen") && <WindowMsn close={() => setShowMessage(false)} />}
                         <div className="md:flex relative pt-38 md:pt-38 lg:pt-24">
                             <img src="/images/trint.png" alt="trin" className="w-40 md:scale-140" />
                             <img src="/images/emo.png" alt="emo" className="w-40 md:scale-140 md:ml-8" />
@@ -83,26 +81,36 @@ export const Main = () => {
                             <img src="/images/foto-home.png" alt="home" className="w-80 px-2 md:scale-140 translate-x-2" />
                             <img src="/images/mao-rock.png" alt="papel" className="absolute w-30 z-0 md:-translate-y-2 scale-70 md:scale-100 -translate-y-10 -right-1 md:-right-10.5 rotate-25" />
                         </div>
-                        {showNotification && !showMessage && (
+                        {showNotification && !showMessage && (currentGuest?.slug !== "rosa-e-gi" && currentGuest?.slug !== "karen") && (
                             <WindowNotification openNotification={openMessageWindow} />
                         )}
                         <img src="/images/papel.png" alt="papel" className="absolute -right-20 -bottom-45" />
                         <img src="/images/papel.png" alt="papel" className="absolute -left-20 -top-45 rotate-180" />
                         <audio ref={audioRef} src="/audios/notificacao.mp3" />
-                        {currentGuest?.presenca == null || currentGuest?.presenca === false ? (
-                            <div className="text-black text-center z-99 mt-12">
-                                <h2 className="rocket-font text-center bg-white px-3 py-2 border border-gray-600 z-99" style={{ fontSize: "23px" }}>Confirmar até dia 10 de Novembro</h2>
-                                <h2 className="rocket-font shadow text-center" style={{ fontSize: "27px" }}>Faltam:</h2>
-                                <Countdown
-                                    date={new Date(2025, 10, 10, 23, 59, 0)}
-                                    renderer={({ days, hours, minutes, seconds }) => (
-                                        <span className="text-white px-2 py-1 rocket-font bg-black">{days}d {hours}h {minutes}m {seconds}s</span>
-                                    )}
-                                />
+                        {(currentGuest?.slug === "rosa-e-gi" || currentGuest?.slug === "karen") ? (
+                            <div className="text-black text-center">
+                                {currentGuest?.slug !== "rosa-e-gi" && currentGuest?.slug !== "karen" ? (
+                                    <span className="text-white px-4 block py-2 rocket-font mb-2 bg-black">
+                                        <FaCheck className="inline" /> confirmado
+                                    </span>
+                                ) : (
+                                    <span className="text-white px-4 block py-2 rocket-font mb-2 bg-black pr-10 mr-4">Convite expirado
+                                    </span>
+                                )}
                             </div>
                         ) : (
-                            <div className="text-black text-center">
-                                <span className="text-white px-4 block py-2 rocket-font mb-2 bg-black"><FaCheck className="inline" /> confirmado</span>
+                            <div className="text-black text-center z-99 mt-12">
+                                {currentGuest?.presenca === true ? (
+                                    <span className="text-white px-4 block py-2 rocket-font mb-2 bg-black">
+                                        <FaCheck className="inline" /> confirmado
+                                    </span>
+                                ) : (
+                                    <>
+                                        <h2 className="rocket-font text-center bg-white px-3 py-2 border border-gray-600 z-99" style={{ fontSize: "23px" }}>
+                                            Dia 13 de Dezembro ás 16h
+                                        </h2>
+                                    </>
+                                )}
                             </div>
                         )}
                     </div>
